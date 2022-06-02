9c9b9686a07238d2046b0dbfe43baca419da5048
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-06-02 16:07:37 +0700

    test

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ fetch('https://share.traffy.in.th/js/map_chadchart.js')
         text = text.replace(/img\//g, 'https://share.traffy.in.th/img/');
         fs.writeFileSync('map_chadchart.js', text);
     })
+
+fetch('https://share.traffy.in.th/css/style_chadchart.css')
+.then(res => res.text())
+    .then(text => {
+        //change in text from .. to https://share.traffy.in.th/
+        text = text.replace(/..\//g, 'https://share.traffy.in.th/');
+        fs.writeFileSync('style_chadchart.css', text);
+    })

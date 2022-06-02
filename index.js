3b77ec9e93625c8e47f6a7dcbd99a60e0d3f7e27
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-06-02 21:10:38 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const fs = require('fs');
 
-fetch('https://share.traffy.in.th/js/map_chadchart.js')
+/*fetch('https://share.traffy.in.th/js/map_chadchart.js')
     .then(res => res.text())
     .then(text => {
         //change in text from img/ to https://share.traffy.in.th/img/
@@ -15,7 +15,7 @@ fetch('https://share.traffy.in.th/css/style_chadchart.css')
         //change in text from ../fonts/ to https://share.traffy.in.th/fonts/
         text = text.replace(/\.\.\/fonts\//g, 'https://share.traffy.in.th/fonts/');
         fs.writeFileSync('style_chadchart.css', text);
-    })
+    })*/
 
 fetch('https://publicapi.traffy.in.th/share/teamchadchart/geojson')
     .then(res => res.json())

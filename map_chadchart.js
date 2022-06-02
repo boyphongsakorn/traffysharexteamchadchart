9e8945f61091b1f604ec4edfd5035bc5bdf78287
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-06-03 00:15:16 +0700

    test

--- map_chadchart.js
+++ map_chadchart.js
@@ -131,7 +131,7 @@ async function searchPost(trendProblemtype=undefined) {
         // console.log("else trendProblemtype")
     }
 
-    if (text_search == "ประเภท:ทั้งหมด" && text_search == "" && filterDistrict == null && filterSubDistrict == null && filterStartDate == null && filterEndDate == null && filterState == null) {
+    if (text_search == "" && filterDistrict == null && filterSubDistrict == null && filterStartDate == null && filterEndDate == null && filterState == null) {
         url_geojson_hashtag = 'https://raw.githubusercontent.com/boyphongsakorn/traffysharexteamchadchart/main/geojson.json'
     }
 
